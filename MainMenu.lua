954fd6d72b2f62189bc88e423be4247b34231d18
Dwyer, Peter <Peter.Dwyer@uk.penguingroup.com>
2014-04-08 13:22:41 +0100

    First commit

---
+++ MainMenu.lua
@@ -0,0 +1,123 @@
+local composer = require("composer")
+
+local scene = composer.newScene()
+
+-- input controller commands and listeners
+local inputDevices = system.getInputDevices()
+local controllers = {}
+
+-- local forward references should go here --
+
+
+---------------------------------------------------------------------------------
+-- BEGINNING OF IMPLEMENTATION
+---------------------------------------------------------------------------------
+
+function onKeyEvent( event )
+    
+end
+
+function onInputdeviceStatusChanged( event )
+    
+end
+
+-- Called when the scene's view does not exist:
+function scene:createScene( event )
+    local group = self.view
+
+    background = display.newImage("images/back3.png", 0 , 0)
+     
+    group:insert(background) 
+end
+
+-- Called BEFORE scene has moved onscreen:
+function scene:enterScene( event )
+    local group = self.view
+    
+    -- check which devices we have (if any)
+    for deviceIndex = 1, #inputDevices do
+        print( deviceIndex, "canVibrate", inputDevices[deviceIndex].canVibrate )
+        print( deviceIndex, "connectionState", inputDevices[deviceIndex].connectionState )
+        print( deviceIndex, "descriptor", inputDevices[deviceIndex].descriptor )
+        print( deviceIndex, "displayName", inputDevices[deviceIndex].displayName )
+        print( deviceIndex, "isConnected", inputDevices[deviceIndex].isConnected )
+        print( deviceIndex, "type", inputDevices[deviceIndex].type )
+        print( deviceIndex, "permenantid", tostring(inputDevices[deviceIndex].permanentId) )
+        print( deviceIndex, "andoridDeviceid", inputDevices[deviceIndex].androidDeviceId ) 
+        
+        if inputDevices[deviceIndex].descriptor == "Joystick 1" then
+            controllers[1] = inputDevices[deviceIndex]
+        elseif inputDevices[deviceIndex].descriptor == "Joystick 2" then
+            controllers[2] = inputDevices[deviceIndex]
+        elseif inputDevices[deviceIndex].descriptor == "Joystick 3" then
+            controllers[3] = inputDevices[deviceIndex]
+        elseif inputDevices[deviceIndex].descriptor == "Joystick 4" then
+            controllers[4] = inputDevices[deviceIndex]
+        end    
+    end
+end
+
+
+-- Called when scene is about to move offscreen:
+function scene:exitScene( event )
+    local group = self.view
+    
+end
+
+
+
+-- Called prior to the removal of scene's "view" (display group)
+function scene:destroyScene( event )
+    local group = self.view
+    
+end
+
+
+-- Called if/when overlay scene is displayed via storyboard.showOverlay()
+function scene:overlayBegan( event )
+    local group = self.view
+    local overlay_name = event.sceneName  -- name of the overlay scene
+    
+end
+
+
+-- Called if/when overlay scene is hidden/removed via storyboard.hideOverlay()
+function scene:overlayEnded( event )
+    local group = self.view
+    local overlay_name = event.sceneName  -- name of the overlay scene
+    
+end
+
+
+---------------------------------------------------------------------------------
+-- END OF IMPLEMENTATION
+---------------------------------------------------------------------------------
+
+-- "createScene" event is dispatched if scene's view does not exist
+scene:addEventListener( "createScene", scene )
+
+-- "enterScene" event is dispatched whenever scene transition has finished
+scene:addEventListener( "enterScene", scene )
+
+-- "exitScene" event is dispatched before next scene's transition begins
+scene:addEventListener( "exitScene", scene )
+
+-- "destroyScene" event is dispatched before view is unloaded, which can be
+-- automatically unloaded in low memory situations, or explicitly via a call to
+-- storyboard.purgeScene() or storyboard.removeScene().
+scene:addEventListener( "destroyScene", scene )
+
+-- "overlayBegan" event is dispatched when an overlay scene is shown
+scene:addEventListener( "overlayBegan", scene )
+
+-- "overlayEnded" event is dispatched when an overlay scene is hidden/removed
+scene:addEventListener( "overlayEnded", scene )
+
+---------------------------------------------------------------------------------
+
+Runtime:addEventListener( "key", onKeyEvent )
+Runtime:addEventListener( "inputDevicestatus", onInputdeviceStatusChanged )
+
+return scene
+
+
