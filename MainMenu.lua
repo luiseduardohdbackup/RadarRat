dd0f01f23e242d2fa721da73cc9d7655ce12e9ee
PeterDwyer <penhalion@btinternet.com>
2014-05-12 13:46:40 +0100

    fleshing out main menu

--- MainMenu.lua
+++ MainMenu.lua
@@ -1,93 +1,127 @@
+-- required includes should go here --
 local composer = require("composer")
 
+-- local vars should go here --
 local scene = composer.newScene()
+local joystickEnsbled = false
 
 -- input controller commands and listeners
 local inputDevices = system.getInputDevices()
 local controllers = {}
 
--- local forward references should go here --
+-- forward references should go here --
 
 
 ---------------------------------------------------------------------------------
 -- BEGINNING OF IMPLEMENTATION
 ---------------------------------------------------------------------------------
 
+---------------------------------------------------------------------------
+-- onKeyEvent
+-- 
+-- Called BEFORE scene has moved onscreen:
+-- 
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function onKeyEvent( event )
     
 end
 
+---------------------------------------------------------------------------
+-- onInputdeviceStatusChanged
+-- 
+-- Called BEFORE scene has moved onscreen:
+-- 
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function onInputdeviceStatusChanged( event )
     
 end
 
--- Called when the scene's view does not exist:
+---------------------------------------------------------------------------
+-- create
+-- 
+-- Called if scene has been not yet been created:
+-- 
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function scene:create( event )
     local group = self.view
-
-    --background = display.newImage("images/back3.png", 0 , 0)
-    print("testing")
-    --group:insert(background) 
 end
 
--- Called BEFORE scene has moved onscreen:
+---------------------------------------------------------------------------
+-- Show
+-- 
+-- Called BEFORE scene has moved onscreen: event.phase == "will"
+-- Called after scene has moved on screem: event.phase == "did"
+-- 
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function scene:show( event )
     local group = self.view
     
-    -- check which devices we have (if any)
-    for deviceIndex = 1, #inputDevices do
-        print( deviceIndex, "canVibrate", inputDevices[deviceIndex].canVibrate )
-        print( deviceIndex, "connectionState", inputDevices[deviceIndex].connectionState )
-        print( deviceIndex, "descriptor", inputDevices[deviceIndex].descriptor )
-        print( deviceIndex, "displayName", inputDevices[deviceIndex].displayName )
-        print( deviceIndex, "isConnected", inputDevices[deviceIndex].isConnected )
-        print( deviceIndex, "type", inputDevices[deviceIndex].type )
-        print( deviceIndex, "permenantid", tostring(inputDevices[deviceIndex].permanentId) )
-        print( deviceIndex, "andoridDeviceid", inputDevices[deviceIndex].androidDeviceId ) 
+    -- We only want to execute the controller check once.
+    if event.phase == "will" then
+        -- check which devices we have (if any)
+        for deviceIndex = 1, #inputDevices do
+            print( deviceIndex, "canVibrate", inputDevices[deviceIndex].canVibrate )
+            print( deviceIndex, "connectionState", inputDevices[deviceIndex].connectionState )
+            print( deviceIndex, "descriptor", inputDevices[deviceIndex].descriptor )
+            print( deviceIndex, "displayName", inputDevices[deviceIndex].displayName )
+            print( deviceIndex, "isConnected", inputDevices[deviceIndex].isConnected )
+            print( deviceIndex, "type", inputDevices[deviceIndex].type )
+            print( deviceIndex, "permenantid", tostring(inputDevices[deviceIndex].permanentId) )
+            print( deviceIndex, "andoridDeviceid", inputDevices[deviceIndex].androidDeviceId ) 
+
+            if inputDevices[deviceIndex].descriptor == "Joystick 1" then
+                controllers[1] = inputDevices[deviceIndex]
+            elseif inputDevices[deviceIndex].descriptor == "Joystick 2" then
+                controllers[2] = inputDevices[deviceIndex]
+            elseif inputDevices[deviceIndex].descriptor == "Joystick 3" then
+                controllers[3] = inputDevices[deviceIndex]
+            elseif inputDevices[deviceIndex].descriptor == "Joystick 4" then
+                controllers[4] = inputDevices[deviceIndex]
+            end    
+        end
         
-        if inputDevices[deviceIndex].descriptor == "Joystick 1" then
-            controllers[1] = inputDevices[deviceIndex]
-        elseif inputDevices[deviceIndex].descriptor == "Joystick 2" then
-            controllers[2] = inputDevices[deviceIndex]
-        elseif inputDevices[deviceIndex].descriptor == "Joystick 3" then
-            controllers[3] = inputDevices[deviceIndex]
-        elseif inputDevices[deviceIndex].descriptor == "Joystick 4" then
-            controllers[4] = inputDevices[deviceIndex]
-        end    
+        -- If we detect a controller then enaberl controller options
+        if controllers[1] ~= nil then
+            joystickEnabled = true
+        end
     end
 end
 
 
--- Called when scene is about to move offscreen:
+---------------------------------------------------------------------------
+-- hide
+-- 
+-- Called BEFORE scene has moved off screen:event.phase == "will"
+-- Called when scene has moved off screen: event.phase == "did" 
+--
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function scene:hide( event )
     local group = self.view
     
 end
 
-
-
--- Called prior to the removal of scene's "view" (display group)
+---------------------------------------------------------------------------
+-- destroy
+-- 
+-- Called when scene is destroyed:
+-- 
+-- @param event
+-- @return
+---------------------------------------------------------------------------
 function scene:destroy( event )
     local group = self.view
     
 end
-
-
--- Called if/when overlay scene is displayed via storyboard.showOverlay()
-function scene:overlayBegan( event )
-    local group = self.view
-    local overlay_name = event.sceneName  -- name of the overlay scene
-    
-end
-
-
--- Called if/when overlay scene is hidden/removed via storyboard.hideOverlay()
-function scene:overlayEnded( event )
-    local group = self.view
-    local overlay_name = event.sceneName  -- name of the overlay scene
-    
-end
-
 
 ---------------------------------------------------------------------------------
 -- END OF IMPLEMENTATION
@@ -95,21 +129,16 @@ end
 
 -- "createScene" event is dispatched if scene's view does not exist
 scene:addEventListener( "create", scene )
-
 -- "enterScene" event is dispatched whenever scene transition has finished
 scene:addEventListener( "show", scene )
-
 -- "exitScene" event is dispatched before next scene's transition begins
 scene:addEventListener( "hide", scene )
-
 -- "destroyScene" event is dispatched before view is unloaded, which can be
 -- automatically unloaded in low memory situations, or explicitly via a call to
 -- storyboard.purgeScene() or storyboard.removeScene().
 scene:addEventListener( "destroy", scene )
 
-
 ---------------------------------------------------------------------------------
-
 Runtime:addEventListener( "key", onKeyEvent )
 Runtime:addEventListener( "inputDevicestatus", onInputdeviceStatusChanged )
 
--- main.lua
+++ main.lua
@@ -4,47 +4,16 @@ display.setDefault( "magTextureFilter", "nearest" )
 display.setDefault( "minTextureFilter", "nearest" )
 system.activate("multitouch")
 
+-- required includes go here --
 local mte = require("mte").createMTE()
 local composer = require("composer")
+local analytics = require("analytics")
 
--- forward declarations
+-- forward declarations go here --
 
--- local variables
+-- local variables go here --
 local movement = nil
 
 -- Attach this instance of mte to the composer
 composer.mte = mte
 composer.gotoScene("MainMenu")
--- Movement functions 
-local move = function( event )
-	if event.phase == "began" then
-		display.getCurrentStage():setFocus(event.target, event.id)
-		event.target.isFocus = true
-	end
-    
-	if event.phase == "began" or event.phase == "moved" then
-		local dirX = event.x - event.target.x
-		local dirY = event.y - event.target.y	
-		local angle = math.deg(math.atan(dirY/dirX))
-		if dirX < 0 then
-			angle = 90 + (90 - (angle * -1))
-		end
-		angle = angle + 90
-		angle = math.round(angle / 90) * 90
-		if angle == 360 then
-			angle = 0
-		end
-		if angle % 90 ~= 0 then
-			movement = nil
-		else
-			movement = tostring(angle)
-		end
-	elseif event.phase == "ended" or event.phase == "cancelled" then
-		movement = nil
-		display.getCurrentStage():setFocus( event.target, nil )
-		event.target.isFocus = false
-	end
-	return true
-end
-
-
